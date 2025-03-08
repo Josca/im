27ec9cffaba43aeebe654266a1fc37de37f91d98
Josef Hak <pepa.hak@gmail.com>
2025-03-08 12:16:00 +0100

    Bump version 1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='Fast and simple console image processing tool.',
     author='Josef Hák',
     author_email='pepa.hak@gmail.com',
-    version='1.1.0',
+    version='1.2.0',
     packages=['im'],
     url='https://github.com/Josca/im',
     keywords=['image', 'viewer', 'crop', 'convert', 'optimize jpeg'],
